bc660f1d08c24f03cddefafc551b49aa43ef0e88
AlexanderSeleznev <seleznevam84@ya.ru>
2021-06-15 15:46:48 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+    application
+}
+
+group = "com.syrupmg"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
